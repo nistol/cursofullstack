c5b8a5840227271bd4ba2688b0cb6d11de02a097
maxi <mnistol@gmail.com>
2022-08-12 10:20:31 -0300

    a

--- PRACTICA 3/java.js
+++ PRACTICA 3/java.js
@@ -1,27 +1,29 @@
 
 window.onload = inicio;
+const NOMBRE = "maxi";
+const PASSCORRECTA= "nistol";
 
 
 function inicio() {
-    document.querySelector(".contenedor").insertAdjacentHTML("beforeend", `   
-    <input id="inp" type="text">
-    <button id="bot"> Boton </button>
-    <div id="contenido"> </div>`);
-
     const boton = document.getElementById("bot");
-    boton.onclick=leer;
+    boton.onclick = leer;
     var contenido = document.getElementById("contenido");
-    
-   
+
+
 }
 function leer() {
     let max = document.getElementById("inp").value;
+    let pass= document.getElementById("pass").value;
     let input = document.getElementById("inp");
-    if (max == "maxi"){
-        contenido.innerHTML = `hola ${max}`;
-    }else {
-        contenido.innerHTML ="";
-        input.style.background="blue";
+    let nombreM = max.toLowerCase();
+    let nombreMi = max.substr(0, 1).toUpperCase() + max.substr(1).toLowerCase();
+    if (nombreM == NOMBRE && pass == PASSCORRECTA ) {
+        contenido.innerHTML = `hola ${nombreMi}`;
+       location.href = "https://mnsitol.000webhostapp.com/";
+    } else {
+        contenido.innerHTML = "Usuario o contraseña incorrectos";
+        input.value = "";
+        input.focus();
+
     }
-        
 }
