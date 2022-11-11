ffcdd5e474e768fba6daedb34937885a98f4e5ff
maxi <mnistol@gmail.com>
2022-11-11 10:56:57 -0300

    a

--- SITIO/BACK/modelos/bd.js
+++ SITIO/BACK/modelos/bd.js
@@ -6,7 +6,8 @@ var pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME
+    database: process.env.MYSQL_DB_NAME,
+    port:process.env.MYSQL_DB_PORT
 })
 
 pool.query = util.promisify(pool.query);
